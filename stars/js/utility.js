22e2d6bdd556e41a5a113ae23fd2692e0905e1a7
Eli McIlveen <eli@4ge.ca>
2014-03-05 22:45:34 -0500

    Utility: deepExtend().

--- stars/js/utility.js
+++ stars/js/utility.js
@@ -27,10 +27,23 @@ var edm = (function () {
 			}
 		},
 	 
-		extend: function (destination, extension) {
-			for ( var prop in extension ){
-				destination[prop] = extension[prop];
+		extend: function (destination, source) {
+			for (var prop in source) {
+				destination[prop] = source[prop];
 			}
+		},
+
+		// after http://andrewdupont.net/2009/08/28/deep-extending-objects-in-javascript/
+		deepExtend : function(destination, source) {
+		  	for (var prop in source) {
+			    if (source[prop] && source[prop].constructor && source[prop].constructor === Object) {
+				    destination[prop] = destination[prop] || {};
+					arguments.callee(destination[prop], source[prop]);
+			    } else {
+		      		destination[prop] = source[prop];
+		    	}
+		  	}
+		  	return destination;
 		},
 
 		// Angle of a point from the origin. 0 = positive x axis ("east")
